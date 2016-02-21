76b6d8c23a41b00483163b334789cee5d101ed75
Gregg Fisher <gregg@quikklymobile.com>
2016-02-21 21:37:25 +0000

    Initial Commit

---
+++ QuikklyiOSTechTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuikklyiOSTechTest
+//
+//  Created by Gregg Fisher on 21/02/2016.
+//  Copyright © 2016 Quikkly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuikklyiOSTechTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QuikklyiOSTechTest
+//
+//  Created by Gregg Fisher on 21/02/2016.
+//  Copyright © 2016 Quikkly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
